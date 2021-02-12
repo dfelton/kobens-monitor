cecff75db52ab7e72d1743202cb45503bd0fe9b4
Darren Felton <darren_felton@hotmail.com>
2021-02-12 01:14:35 +0000

    Default don't refresh; comment out resource monitors

--- index.php
+++ index.php
@@ -9,16 +9,17 @@
     <div class="page">
         <h2>Orders</h2>
         <div class="container-fluid form-group">
-                    <label for="refresh-orders">Auto Refresh Order Data:</label>
-                    <select id="refresh-orders" class="form-control-sm">
-                        <option selected value="1">Yes</option>
-                        <option value="0">No</option>
-                    </select>
+            <label for="refresh-orders">Auto Refresh Order Data:</label>
+            <select id="refresh-orders" class="form-control-sm">
+                <option value="1">Yes</option>
+                <option selected value="0">No</option>
+            </select>
         </div>
         <div class="container-fluid monitors clear-after" id="orders" >
             <img src="/img/loading.gif" class="spinner"/>
         </div>
 
+        <!--
         <h2>Resource Monitors</h2>
         <div class="container-fluid form-group">
             <label for="refresh_cpuMemory">Auto Refresh Resource Data:</label>
@@ -36,10 +37,13 @@
         <div class="container-fluid monitors clear-after" id="memory" >
             <img src="/img/loading.gif" class="spinner"/>
         </div>
+        -->
     </div>
     <script src="/js/assets/jquery-3.3.1.min.js"></script>
     <script src="/js/assets/canvasjs.min.js"></script>
     <script src="/js/monitor-orders.js"></script>
+    <!--
     <script src="/js/monitor-cpu-memory.js"></script>
+    -->
 </body>
 </html>
