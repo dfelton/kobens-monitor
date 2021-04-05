1cc15be4f832c33d7c09674073fd73122511f373
Darren Felton <darren_felton@hotmail.com>
2021-04-05 04:34:47 +0000

    fix some stuff

--- orders-pie.php
+++ orders-pie.php
@@ -2,18 +2,19 @@
 <html>
 <head>
     <title>xkcd.com/1732/</title>
-    <link rel="stylesheet" href="/css/bootstrap.min.css"/>
-    <link rel="stylesheet" href="/css/styles.css"/>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/styles.css"/>
 </head>
 <body>
     <div class="page">
+        <p><a href="/" class="home">Home</a></p>
     	<input type="checkbox" checked="checked" id="refresh"/><label for="refresh">Auto Refresh</label>
         <div class="container-fluid monitors clear-after" id="orders">
-            <img src="/img/loading.gif" class="spinner"/>
+            <img src="img/loading.gif" class="spinner"/>
         </div>
     </div>
-    <script src="/js/assets/jquery-3.3.1.min.js"></script>
-    <script src="/js/assets/canvasjs.min.js"></script>
-    <script src="/js/monitor-orders-pie-charts.js"></script>
+    <script src="js/assets/jquery-3.3.1.min.js"></script>
+    <script src="js/assets/canvasjs.min.js"></script>
+    <script src="js/monitor-orders-pie-charts.js"></script>
 </body>
 </html>
